632a1eaa88f9da041f43f873dbd4f8911650ea63
IshaanCoderIsGood <bajruamby@gmail.com>
2024-12-26 18:56:47 +0530

    act 1

---
+++ act 1.py
@@ -0,0 +1,13 @@
+def get1(a,a_size):
+    counter = 0
+    maxone = 0
+    for i in range(0, a_size):
+        if(a[i] == 0):
+            counter = 0
+        else:
+            counter += 1
+            maxone = max(maxone, counter)
+    return maxone
+a = [1,0,0,0,1,1,1,1,0,1,1,1,1,1,1,1]
+a_size = len(a)
+print(get1(a,a_size))
